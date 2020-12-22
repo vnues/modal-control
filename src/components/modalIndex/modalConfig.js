0976e15f07c7770493a9fbbc6c3905f72d58f171
wanggangfeng <wanggangfeng-jk@360shuke.com>
2020-12-22 15:17:56 +0800

    fix: 第二版

--- src/components/modalIndex/modalConfig.js
+++ src/components/modalIndex/modalConfig.js
@@ -6,9 +6,6 @@ export default {
       id: 1, // 弹框的id
       name: 'modalA',
       level: 100,
-      // 弹框的优先级
-      // 由前端控制弹框是否显示
-      // 当我们一个活动过去了废弃一个弹框时候，可以不需要通过后端去更改
       frontShow: true,
       apiFlag: ['mockA_1', 'mockA_2']
     }, {
