462ae2f1a47815a64c0c635fad02f49e9879a479
wanggangfeng <wanggangfeng-jk@360shuke.com>
2020-12-22 14:09:45 +0800

    fix: 第一版

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 import router from './router'
-
-Vue.config.productionTip = false;
-
-Vue.use(Antd);
 
 Vue.config.productionTip = false
 
+Vue.use(Antd)
+
+Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
